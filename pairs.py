166a425b9d73cdd2bf885a604d197387cfda1ed4
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-08-29 09:24:23 +0530

    Create pairs.py

---
+++ pairs.py
@@ -0,0 +1,3 @@
+'''pairs
+You will be given an array of integers and a target value. Determine the number of pairs of array elements that have a difference equal to a target value.
+For example, given an array of [1, 2, 3, 4] and a target value of 1, we have three values meeting the condition: 2-1=1,3-2=1 , and4-3=1 .'''
